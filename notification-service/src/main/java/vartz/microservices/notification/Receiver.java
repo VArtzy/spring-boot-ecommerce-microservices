bb845dc5ebec7d7a706b6951c5d0fe159a066638
VArtz <farrelnikoson@gmail.com>
2024-06-18 19:18:34 +0700

    adding message queue rabbitmq using notification service as consumer and order service as producer

--- notification-service/src/main/java/vartz/microservices/notification/Receiver.java
+++ notification-service/src/main/java/vartz/microservices/notification/Receiver.java
@@ -4,14 +4,28 @@ import java.util.concurrent.CountDownLatch;
 
 import org.springframework.stereotype.Component;
 
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.tracing.Tracer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @Component
+@Slf4j
+@RequiredArgsConstructor
 public class Receiver {
+
+    private final ObservationRegistry registry;
+    private final Tracer tracer;
 
     private CountDownLatch latch = new CountDownLatch(1);
 
     public void receiveMessage(String message) {
-        System.out.println("Received message: " + message);
-        latch.countDown();
+        Observation.createNotStarted("on-message", this.registry).observe(() -> {
+            log.info("Received order: {}", message);
+            log.info("Tracing: {}", this.tracer.currentSpan().context().traceId());
+            latch.countDown();
+        });
     }
 
     public CountDownLatch getLatch() {
